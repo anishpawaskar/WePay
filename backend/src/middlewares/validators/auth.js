90692e17ac628fde68ad1415ba23a295178ba2a0
Anish Pawaskar <anishpawaskar@gmail.com>
2024-09-27 01:29:42 +0530

    feat: create validator to validate sign in payload and add it in signin route

--- backend/src/middlewares/validators/auth.js
+++ backend/src/middlewares/validators/auth.js
@@ -14,6 +14,23 @@ export const signupUserValidator = async (req, res, next) => {
     await schema.validateAsync(body);
     next();
   } catch (err) {
+    console.log(`Error validating sigupUser payload: ${err}`);
+    res.status(411).json({ message: err.details[0].message });
+  }
+};
+
+export const signinUserValidator = async (req, res, next) => {
+  const body = req.body;
+
+  const schema = joi.object({
+    username: joi.string().email().required(),
+    password: joi.string().required(),
+  });
+
+  try {
+    await schema.validateAsync();
+    next();
+  } catch (err) {
     console.log(`Error validating siginUser payload: ${err}`);
     res.status(411).json({ message: err.details[0].message });
   }
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { signinUser, signupUser } from "../controllers/auth.js";
-import { signupUserValidator } from "../middlewares/validators/auth.js";
+import {
+  signinUserValidator,
+  signupUserValidator,
+} from "../middlewares/validators/auth.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUserValidator, signupUser);
-router.post("/signin", signinUser);
+router.post("/signin", signinUserValidator, signinUser);
 
 export default router;
